c8777ac429d567f3cf0fd9b38fda8bf94c665e2d
Rahul Wermund <rahulcd@gmail.com>
2022-07-24 11:29:28 +0200

    Added complete init.sql and update scripts

---
+++ sql/create_update_triggers.sql
@@ -0,0 +1,32 @@
+-- Update triggers per table
+CREATE TRIGGER update_country_last_update_time
+BEFORE UPDATE ON country
+FOR EACH ROW EXECUTE PROCEDURE update_last_updated_column();
+
+CREATE TRIGGER update_city_last_update_time
+BEFORE UPDATE ON city
+FOR EACH ROW EXECUTE PROCEDURE update_last_updated_column();
+
+CREATE TRIGGER update_fleet_last_update_time
+BEFORE UPDATE ON fleet
+FOR EACH ROW EXECUTE PROCEDURE update_last_updated_column();
+
+CREATE TRIGGER update_airport_last_update_time
+BEFORE UPDATE ON airport
+FOR EACH ROW EXECUTE PROCEDURE update_last_updated_column();
+
+CREATE TRIGGER update_manufacturer_last_update_time
+BEFORE UPDATE ON manufacturer
+FOR EACH ROW EXECUTE PROCEDURE update_last_updated_column();
+
+CREATE TRIGGER update_airplane_last_update_time
+BEFORE UPDATE ON airplane
+FOR EACH ROW EXECUTE PROCEDURE update_last_updated_column();
+
+CREATE TRIGGER update_fleet_airplane_last_update_time
+BEFORE UPDATE ON fleet_airplane FOR EACH ROW EXECUTE PROCEDUR
+E update_last_updated_column();
+
+CREATE TRIGGER update_fleet_route_last_update_time
+BEFORE UPDATE ON fleet_route FOR EACH ROW EXECUTE PROCEDURE u
+pdate_last_updated_column();
--- sql/init.sql
+++ sql/init.sql
@@ -2,3 +2,86 @@
 CREATE DATABASE flights;
 
 -- Create country table
+CREATE TABLE country (
+	id serial NOT NULL,
+	PRIMARY KEY (id),
+	name character varying NOT NULL,
+	iso2 character varying (2) NOT NULL,
+	iso3 character varying (3) NOT NULL,
+	country_code integer NOT NULL,
+	domain_name character varying (10),
+	main_language character varying NOT NULL,
+	secondary_language character varying,
+	tertiary_language character varying,
+	currency character varying NOT NULL,
+	nationality character varying NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create city table
+CREATE TABLE city (
+	id serial NOT NULL,
+	PRIMARY KEY (id),
+	name character varying NOT NULL,
+	country_id integer REFERENCES country (id),
+	capital boolean NOT NULL,
+	population integer NOT NULL,
+	latitude numeric NOT NULL,
+	longitude numeric NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create fleet table
+CREATE TABLE fleet (
+	id serial NOT NULL,
+	PRIMARY KEY (id),
+	name character varying NOT NULL,
+	call_name character varying,
+	hub_at integer REFERENCES airport (id),
+	country_id integer REFERENCES country (id),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create airport table
+CREATE TABLE airport (
+	id serial NOT NULL,
+	PRIMARY KEY (id),
+	name character varying NOT NULL,
+	icao character varying (4) NOT NULL,
+	iata character varying (3) NOT NULL,
+	city_id integer REFERENCES city (id),
+	country_id integer REFERENCES country (id),
+	hub_to integer REFERENCES fleet (id),
+	number_of_runways integer NOT NULL,
+	number_of_terminals integer NOT NULL,
+	capacity integer,
+	international boolean NOT NULL,
+	junction boolean NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create manufacturer table
+CREATE TABLE manufacturer (
+	id serial NOT NULL,
+	PRIMARY KEY (id),
+	name character varying NOT NULL,
+	based_in integer REFERENCES city (id),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create airplane table
+CREATE TABLE airplane (
+	id serial NOT NULL,
+	PRIMARY KEY (id),
+	name character varying NOT NULL,
+	manufacturer_id integer REFERENCES manufacturer (id),
+	capacity integer NOT NULL,
+	max_range_km  integer NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
