75997c95abe84b724f36db1aae4c18422e122bb6
Rahul Wermund <163890411+rahulwermunddmde@users.noreply.github.com>
2024-03-21 23:07:24 +0000

    Move flyway migration to separate function such that we can use it "without effects" in test code

--- src/main/scala/flightdatabase/db/DbInitiation.scala
+++ src/main/scala/flightdatabase/db/DbInitiation.scala
@@ -34,10 +34,10 @@ object DbInitiation {
     * @param config The database configuration.
     * @param clean  Indicates whether to clean the database before initialization.
     * @tparam F The effect type, which must have an instance of `Async`.
-    * @return The created transactor.
+    * @return The created transactor after Flyway migration.
     */
   def simpleTransactor[F[_]: Async](config: DatabaseConfig, clean: Boolean): Transactor[F] = {
-    databaseInitialisation[F](config, clean).use_
+    flywayMigration(config, clean)
 
     Transactor.fromDriverManager[F](
       config.driver,
@@ -55,19 +55,28 @@ object DbInitiation {
     * @tparam F The effect type, which must have an instance of `Async`.
     * @return A resource that encapsulates the database initialization process.
     */
-  def databaseInitialisation[F[_]: Async](config: DatabaseConfig, clean: Boolean = false)(
-    implicit F: Async[F]
+  def databaseInitialisation[F[_]: Async](
+    config: DatabaseConfig,
+    clean: Boolean
   ): Resource[F, Boolean] =
-    Resource.eval {
-      F.delay {
-        val flyway = Flyway
-          .configure()
-          .cleanDisabled(!clean)
-          .dataSource(config.url, config.access.username, config.access.password)
-          .baselineVersion(config.baseline)
-          .load()
-        if (clean) flyway.clean()
-        flyway.migrate().success
-      }
-    }
+    Resource.pure(flywayMigration(config, clean))
+
+  /**
+    * Performs Flyway migration for the database.
+    *
+    * @param config The database configuration.
+    * @param clean  Flag indicating whether to clean the database before migration.
+    *               Defaults to false.
+    * @return True if the migration is successful, false otherwise.
+    */
+  def flywayMigration(config: DatabaseConfig, clean: Boolean = false): Boolean = {
+    val flyway = Flyway
+      .configure()
+      .cleanDisabled(!clean)
+      .dataSource(config.url, config.access.username, config.access.password)
+      .baselineVersion(config.baseline)
+      .load()
+    if (clean) flyway.clean()
+    flyway.migrate().success
+  }
 }
