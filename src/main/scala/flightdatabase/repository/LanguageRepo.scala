854b29920ad7c1143d62624d4d1242053fdc6365
Rahul Wermund <163890411+rahulwermunddmde@users.noreply.github.com>
2024-03-30 14:39:00 +0000

    Add first class; wip

---
+++ src/main/scala/flightdatabase/repository/LanguageRepo.scala
@@ -0,0 +1,11 @@
+package flightdatabase.repository
+
+// This is a repository that includes all the CRUD operations 
+// pertaining to the Language table
+
+// TODO: Implement the CRUD operations for the Language table
+// TODO: Create an instance of this class in the main application
+//      and use it in the endpoints
+// Move the transactor to the constructor of each repository
+
+class LanguageRepo
