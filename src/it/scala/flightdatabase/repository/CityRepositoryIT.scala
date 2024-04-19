ed6b2c7846449904ee1e01d86f6b38368b6c2ac9
Rahul Wermund <rahul.wermund@dm.de>
2024-04-19 18:17:04 +0200

    Update `CityRepositoryIT` (tests still incomplete)

--- src/it/scala/flightdatabase/repository/CityRepositoryIT.scala
+++ src/it/scala/flightdatabase/repository/CityRepositoryIT.scala
@@ -2,81 +2,126 @@ package flightdatabase.repository
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import flightdatabase.domain.ApiResult
+import flightdatabase.domain.EntryNotFound
 import flightdatabase.domain.city.City
+import flightdatabase.domain.city.CityCreate
 import flightdatabase.testutils.RepositoryCheck
 
 class CityRepositoryIT extends RepositoryCheck {
 
+  lazy val repo: CityRepository[IO] = CityRepository.make[IO].unsafeRunSync()
+
+  val originalCities: List[City] = List(
+    City(
+      1,
+      "Bangalore",
+      1,
+      capital = false,
+      13193000,
+      BigDecimal("12.978889"),
+      BigDecimal("77.591667"),
+      "Asia/Kolkata"
+    ),
+    City(
+      2,
+      "Frankfurt am Main",
+      2,
+      capital = false,
+      791000,
+      BigDecimal("50.110556"),
+      BigDecimal("8.682222"),
+      "Europe/Berlin"
+    ),
+    City(
+      3,
+      "Berlin",
+      2,
+      capital = true,
+      3571000,
+      BigDecimal("52.52"),
+      BigDecimal("13.405"),
+      "Europe/Berlin"
+    ),
+    City(
+      4,
+      "Dubai",
+      4,
+      capital = false,
+      3490000,
+      BigDecimal("23.5"),
+      BigDecimal("54.5"),
+      "Asia/Dubai"
+    ),
+    City(
+      5,
+      "Leiden",
+      5,
+      capital = false,
+      125100,
+      BigDecimal("52.16"),
+      BigDecimal("4.49"),
+      "Europe/Amsterdam"
+    ),
+    City(
+      6,
+      "Chicago",
+      6,
+      capital = false,
+      8901000,
+      BigDecimal("41.85003"),
+      BigDecimal("-87.65005"),
+      "America/Chicago"
+    )
+  )
+
+  val countryToIdMap: Map[String, Long] = Map(
+    "India"                -> 1,
+    "Germany"              -> 2,
+    "United Arab Emirates" -> 4,
+    "Netherlands"          -> 5,
+    "United States"        -> 6
+  )
+
+  val idNotPresent: Long = 10
+  val valueNotPresent: String = "Not present"
+  val veryLongIdNotPresent: Long = 1039495454540034858L
+
+  val newCity: CityCreate = CityCreate(
+    "Munich",
+    2,
+    capital = false,
+    1488000,
+    BigDecimal("48.137222"),
+    BigDecimal("11.575556"),
+    "Europe/Berlin"
+  )
+
+  "Checking if a city exists" should "return a valid result" in {
+    def cityExists(id: Long): Boolean = repo.doesCityExist(id).unsafeRunSync()
+    cityExists(1) shouldBe true
+    cityExists(idNotPresent) shouldBe false
+    cityExists(veryLongIdNotPresent) shouldBe false
+  }
+
   "Selecting all cities" should "return the correct detailed list" in {
-    val cities = {
-      for {
-        repo      <- CityRepository.make[IO]
-        allCities <- repo.getCities
-      } yield allCities
-    }.unsafeRunSync().value.value
+    val cities = repo.getCities.unsafeRunSync().value.value
 
     cities should not be empty
-    cities should contain only (
-      City(
-        1,
-        "Bangalore",
-        1,
-        capital = false,
-        13193000,
-        BigDecimal("12.978889"),
-        BigDecimal("77.591667"),
-        "Asia/Kolkata"
-      ),
-      City(
-        2,
-        "Frankfurt am Main",
-        2,
-        capital = false,
-        791000,
-        BigDecimal("50.110556"),
-        BigDecimal("8.682222"),
-        "Europe/Berlin"
-      ),
-      City(
-        3,
-        "Berlin",
-        2,
-        capital = true,
-        3571000,
-        BigDecimal("52.52"),
-        BigDecimal("13.405"),
-        "Europe/Berlin"
-      ),
-      City(
-        4,
-        "Dubai",
-        4,
-        capital = false,
-        3490000,
-        BigDecimal("23.5"),
-        BigDecimal("54.5"),
-        "Asia/Dubai"
-      ),
-      City(
-        5,
-        "Leiden",
-        5,
-        capital = false,
-        125100,
-        BigDecimal("52.16"),
-        BigDecimal("4.49"),
-        "Europe/Amsterdam"
-      ),
-      City(
-        6,
-        "Chicago",
-        6,
-        capital = false,
-        8901000,
-        BigDecimal("41.85003"),
-        BigDecimal("-87.65005"),
-        "America/Chicago"
-      )
-    )
+    cities should contain only (originalCities: _*)
+  }
+
+  it should "return only names if so required" in {
+    val citiesOnlyNames = repo.getCitiesOnlyNames.unsafeRunSync().value.value
+    citiesOnlyNames should not be empty
+    citiesOnlyNames should contain only (originalCities.map(_.name): _*)
+  }
+
+  "Selecting a city by id" should "return the correct entry" in {
+    def cityById(id: Long): ApiResult[City] = repo.getCity(id).unsafeRunSync()
+
+    originalCities.foreach(city => cityById(city.id).value.value shouldBe city)
+    cityById(idNotPresent).left.value shouldBe EntryNotFound(idNotPresent)
+    cityById(veryLongIdNotPresent).left.value shouldBe EntryNotFound(veryLongIdNotPresent)
   }
 }
