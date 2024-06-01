05fb4be21021aab2486e9f2e1f5c4b573e84f1e7
Rahul Wermund <rahul.wermund@dm.de>
2024-06-01 22:32:05 +0200

    Fix AirlineEndpointsTest

--- src/test/scala/flightdatabase/api/endpoints/AirlineEndpointsTest.scala
+++ src/test/scala/flightdatabase/api/endpoints/AirlineEndpointsTest.scala
@@ -2,57 +2,38 @@ package flightdatabase.api.endpoints
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
-import flightdatabase.config.Configuration
 import flightdatabase.domain.airline.AirlineAlgebra
 import org.http4s.HttpApp
 import org.http4s.Method
 import org.http4s.Request
+import org.http4s.Status._
 import org.http4s.Uri
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 final class AirlineEndpointsTest extends AnyFlatSpec with Matchers with MockFactory {
-  val baseUri = Configuration.configUnsafe.apiConfig.flightDbBaseUri
-  val prefix = "/airlines"
   val mockAirlineAlgebra: AirlineAlgebra[IO] = mock[AirlineAlgebra[IO]]
-  val endpoints: HttpApp[IO] = AirlineEndpoints[IO](prefix, mockAirlineAlgebra).endpoints.orNotFound
+
+  val endpoints: HttpApp[IO] =
+    AirlineEndpoints[IO]("/airlines", mockAirlineAlgebra).endpoints.orNotFound
 
   // Valid and invalid IDs
   val validId: Long = 1
   val idNotFound: Long = 1000
 
-  // Status codes
-  val ok: Int = 200
-  val created: Int = 201
-  val noContent: Int = 204
-  val badRequest: Int = 400
-  val notFound: Int = 404
-  val conflict: Int = 409
+  "Checking if an airline exists" should "return the right response code" in {
+    def getResponse(id: Long, exists: Boolean): Int = {
+      (mockAirlineAlgebra.doesAirlineExist _).expects(id).returns(IO.pure(exists)).once()
 
-  "Creating a URI string" should "work properly" in {
-    createUri("1").toString shouldBe s"$baseUri$prefix/1"
+      endpoints
+        .run(Request(method = Method.HEAD, uri = Uri.unsafeFromString(s"/$id")))
+        .unsafeRunSync()
+        .status
+        .code
+    }
+
+    getResponse(validId, exists = true) shouldBe Ok.code
+    getResponse(idNotFound, exists = false) shouldBe NotFound.code
   }
-
-  "Checking if an airline exists" should "return 200 for a successful call" in {
-    (mockAirlineAlgebra.doesAirlineExist _).expects(validId).returns(IO.pure(true)).once()
-
-    val response = endpoints
-      .run(Request(method = Method.HEAD, uri = createUri(validId.toString)))
-      .unsafeRunSync()
-
-    response.status.code shouldBe ok
-  }
-
-  it should "return 404 for a failed call" in {
-    (mockAirlineAlgebra.doesAirlineExist _).expects(idNotFound).returns(IO.pure(false)).once()
-
-    val response = endpoints
-      .run(Request(method = Method.HEAD, uri = createUri(idNotFound.toString)))
-      .unsafeRunSync()
-
-    response.status.code shouldBe notFound
-  }
-
-  private def createUri(suffix: String): Uri = Uri.unsafeFromString(s"$baseUri$prefix/$suffix")
 }
